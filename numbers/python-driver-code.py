6573d1dd46e540159fe83f21b1aaf0b49ad37933
Archa Chandran <77440660+ARCHA2000@users.noreply.github.com>
2022-04-08 13:13:14 +0530

    Update python-driver-code.py

--- numbers/python-driver-code.py
+++ numbers/python-driver-code.py
@@ -2,6 +2,48 @@
 
 def solve(n: int) -> str:
   # Your code goes here
+    l=[]
+s=str(n)
+x=len(s)//2
+print(x)
+if (len(s)%2==0):
+    l=list(s)
+    x=int(x)
+    p=l[:x]
+    q=l[x:]
+   
+    s1 = [str(i) for i in p]
+     
+   
+    res1 = int("".join(s1))
+    s2 = [str(i) for i in q]
+     
+   
+    res2 = int("".join(s2))
+   
+    if res1 > res2 :
+        return "magic number"
+    else:
+        return "normal number"
+else:
+    x=int(x)
+    l=list(s)
+    p=l[:x]
+    q=l[x+1:]
+    s1 = [str(i) for i in p]
+     
+   
+    res1 = int("".join(s1))
+    s2 = [str(i) for i in q]
+     
+   
+    res2 = int("".join(s2))
+   
+    if res1 > res2 :
+        print("magic number")
+    else:
+        return "normal number"
+
   # n is the given input
   return "Special"
 
